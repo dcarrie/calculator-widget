84afb057507cc4d4ec84391d918bd32addf0281e
Darcy Carrie <dcarrie@jahia.com>
2015-02-08 09:09:42 -0600

    fix repository xml for action

--- src/main/java/org/jahia/modules/formfactory/actions/DoComputeAction.java
+++ src/main/java/org/jahia/modules/formfactory/actions/DoComputeAction.java
@@ -28,89 +28,15 @@ public class DoComputeAction extends Action {
     @Override
     public ActionResult doExecute(HttpServletRequest req, RenderContext renderContext, Resource resource, JCRSessionWrapper session, Map<String, List<String>> parameters, URLResolver urlResolver) throws Exception {
 
-        // Get the node who contains the form structure
-        JCRNodeWrapper formStructure = session.getNodeByIdentifier(parameters.get("formId").get(0));
+        //TODO:Implement Action execution
+        //Action execution result
 
-        // Create json to send at the url
-        JSONObject jsonToSend = new JSONObject();
-        jsonToSend.append("formName", formStructure.getName());
-
-        // Register the url of origin of the result
-        jsonToSend.append("origin", req.getRequestURL().toString());
-
-        List<JCRNodeWrapper> stepsList = JCRContentUtils.getChildrenOfType(formStructure, "fcnt:step");
-        for (JCRNodeWrapper stepNode : stepsList) {
-            for (Map.Entry<String, List<String>> entry : parameters.entrySet()) {
-                if (stepNode.hasNode(entry.getKey())) {
-                    JCRNodeWrapper field = stepNode.getNode(entry.getKey());
-
-                    JSONObject jsonCurrentResult = new JSONObject();
-
-                    // Set the label of the field
-                    jsonCurrentResult.put("label", field.getNode("label").getProperty("jsonValue").getString());
-
-                    // Set the result of the field
-                    List<String> values = entry.getValue();
-                    String[] array = values.toArray(new String[values.size()]);
-                    jsonCurrentResult.put("result", array);
-
-                    // Set if the field was mandatory or not
-                    if ((!field.hasNode("validations")) || (field.hasNode("validations") && !field.getNode("validations").hasNode("required"))) {
-                        jsonCurrentResult.put("optional", true);
-                    }
-                    jsonToSend.append("formResponses", jsonCurrentResult);
-                }
-            }
-        }
-
-        // Get the node who contains the form actions
-        JCRNodeWrapper formActions = formStructure.getNode("actions");
-
-        List<JCRNodeWrapper> actionsSendDataToList = JCRContentUtils.getChildrenOfType(formActions, "fcnt:sendDataToUrlAction");
-
-        ActionResult actionResult = new ActionResult(HttpServletResponse.SC_OK);
-        for (JCRNodeWrapper actionSendDataTo : actionsSendDataToList) {
-            JSONObject jsonAnswer = new JSONObject();
-            UrlValidator urlValidator = new UrlValidator();
-            String url = actionSendDataTo.getNode("sendto").getProperty("jsonValue").getString();
-
-            jsonAnswer.append("actionName", resource.getTemplate());
-            jsonAnswer.append("redirectUrl", "");
-
-            if (StringUtils.isNotEmpty(url) && urlValidator.isValid(url)) {
-                HttpClient httpClient = new HttpClient();
-
-                // Get parameter
-                String parameter = actionSendDataTo.getNode("parametername").getProperty("jsonValue").getString();
-
-                PostMethod postMethod = new PostMethod(url);
-                postMethod.getParams().setContentCharset("UTF-8");
-                postMethod.setRequestEntity(new StringRequestEntity(jsonToSend.toString(), "application/json", "UTF-8"));
-                // Set parameter
-                if (StringUtils.isNotEmpty(parameter)) {
-                    postMethod.addParameter(parameter, jsonToSend.toString());
-                }
-                int responseCode = httpClient.executeMethod(postMethod);
-
-                String responseStatus = postMethod.getStatusLine().toString();
-
-                if (responseCode >= 200 && responseCode < 300) {
-                    jsonAnswer.append("code", responseCode);
-                    jsonAnswer.append("status", "success");
-                    jsonAnswer.append("message", "Data : " + jsonToSend + " sent to : " + url + " code : " + responseCode + " Response : " + responseStatus);
-                } else {
-                    jsonAnswer.append("code", responseCode);
-                    jsonAnswer.append("status", "error");
-                    jsonAnswer.append("message", "Data : " + jsonToSend + " not sent to : " + url + " error code : " + responseCode + " Response : " + responseStatus);
-                }
-                actionResult.setJson(jsonAnswer);
-            } else {
-                jsonAnswer.append("status", "error");
-                jsonAnswer.append("message", "Data : " + jsonToSend + " not sent, they have an error with the url provided, please check and correct : " + url);
-
-                actionResult.setJson(jsonAnswer);
-            }
-        }
-        return actionResult;
+        JSONObject jsonAnswer = new JSONObject();
+        jsonAnswer.append("actionName", resource.getTemplate());
+        jsonAnswer.append("status", "success");
+        jsonAnswer.append("code", HttpServletResponse.SC_CREATED);
+        jsonAnswer.append("redirectUrl", "");
+        jsonAnswer.append("message", "The action Do Compute has been called.");
+        return new ActionResult(HttpServletResponse.SC_OK, null, jsonAnswer);
     }
 }
